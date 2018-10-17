7630b4e5133ed34be8ae76bf70f18d9d660c0998
Piotr Łuczak <piotr.andrzej.luczak@gmail.com>
2018-10-17 08:41:08 +0200

    Exercise1.1

--- src/main/java/pl/p/lodz/dmcs/ptoish/Exercise11.java
+++ src/main/java/pl/p/lodz/dmcs/ptoish/Exercise11.java
@@ -1,6 +1,67 @@
+package pl.p.lodz.dmcs.ptoish;
+
+import java.io.*;
+import java.time.Instant;
+import java.util.Date;
+
 public class Exercise11 {
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        Person person = new Person("Jan", "Kowalski", Date.from(Instant.now()), 18);
+        serializeExercise("Exercise11.ser", person);
+        externalizeExercise("Exercise11.ext", person);
+    }
+
+    private static void externalizeExercise( String filename, Person person) {
+        try (FileOutputStream fileOut = new FileOutputStream(filename)) {
+            try (ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+                person.writeExternal(out);
+            }
+            System.out.println("Person externalized");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+        try (FileInputStream fileIn = new FileInputStream(filename)) {
+            Person newPerson = new Person();
+            try (ObjectInputStream in = new ObjectInputStream(fileIn)) {
+                 newPerson.readExternal(in);
+            }
+            System.out.println("Person deexternalized");
+            System.out.println(newPerson.getName());
+            System.out.println(newPerson.getSurname());
+            System.out.println(newPerson.getBirthday());
+            System.out.println(newPerson.getAge());
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Person class not found");
+            c.printStackTrace();
+        }
+    }
+
+    private static void serializeExercise(String filename, Person person) {
+        try (FileOutputStream fileOut = new FileOutputStream(filename)) {
+            try (ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+                out.writeObject(person);
+            }
+            System.out.println("Person serialized");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+        Person newPerson = null;
+        try (FileInputStream fileIn = new FileInputStream(filename)) {
+            try (ObjectInputStream in = new ObjectInputStream(fileIn)) {
+                newPerson = (Person) in.readObject();
+            }
+            System.out.println("Person deserialized");
+            System.out.println(newPerson.getName());
+            System.out.println(newPerson.getSurname());
+            System.out.println(newPerson.getBirthday());
+            System.out.println(newPerson.getAge());
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Person class not found");
+            c.printStackTrace();
+        }
     }
 }
--- src/main/java/pl/p/lodz/dmcs/ptoish/Person.java
+++ src/main/java/pl/p/lodz/dmcs/ptoish/Person.java
@@ -1,4 +1,68 @@
 package pl.p.lodz.dmcs.ptoish;
 
-public class Person {
+import java.io.*;
+import java.util.Date;
+
+public class Person implements Serializable, Externalizable {
+    private String name;
+    private String surname;
+    private Date birthday;
+    private int age;
+
+    public Person(){}
+
+    public Person(String name, String surname, Date birthday, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeUTF(this.name);
+        out.writeUTF(this.surname);
+        out.writeObject(this.birthday);
+        out.writeInt(this.age);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        this.name = in.readUTF();
+        this.surname = in.readUTF();
+        this.birthday = (Date) in.readObject();
+        this.age = in.readInt();
+    }
 }
